0eeefe34efbecff50f88964a4a0ade2c16e6930d
Farhan7rizal <Farhan7rizal@gmail.com>
2024-03-12 21:29:24 +0700

    Cart model, so much logic
    harus teliti lagi memahami codingan ini

---
+++ models/cart.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(path.dirname(require.main.filename), 'data', 'cart.json');
+
+module.exports = class Cart {
+  //   constructor() {
+  //     this.products = [];
+  //     this.totalPrice = 0;
+  //   } //don't really need a contructor, bc the problem we have is the cart itself is not really an object we'll constantly recreate,
+
+  static addProduct(id, productPrice) {
+    // fetch the previous cart
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      // analyze the cart => find existing product
+      const existingProductIndex = cart.products.findIndex(
+        (prod) => prod.id === id
+      );
+      const existingProduct = cart.products[existingProductIndex]; //looking for existing product in data
+
+      let updatedProduct;
+      // add new product / increase quantity
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct]; //add new item
+      }
+
+      cart.totalPrice = cart.totalPrice + +productPrice; //update total price
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log(err);
+      });
+    });
+  }
+};
