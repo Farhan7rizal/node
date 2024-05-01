ccb822331c5514630ef5f877ef5eb06a3727f8b8
Farhan7rizal <Farhan7rizal@gmail.com>
2024-05-01 20:37:17 +0700

    make cart logic in users model

--- models/user.js
+++ models/user.js
@@ -3,14 +3,29 @@ const getDb = require('../util/database').getDb;
 const { ObjectId } = require('mongodb');
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.username = username;
     this.email = email;
+    this.cart = cart; //{items: []}
+    this._id = id;
   }
 
   save() {
     const db = getDb();
     return db.collection('users').insertOne(this);
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //find index which simply is a function javascript will execute for every element in the items array
+    //   return cp._id === product._id;
+    // })
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    db.collection('users').updateOne(
+      { _id: this._id },
+      { $set: { cart: updatedCart } }
+    );
   }
 
   static findById(userId) {
