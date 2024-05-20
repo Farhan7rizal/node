bc69d226e3655e2be3ff5c3a72b28cbce85b4251
Farhan7rizal <Farhan7rizal@gmail.com>
2024-05-20 10:01:39 +0700

    using express-session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,17 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get('cookie').split('=')[1] === 'true';
-  console.log(isLoggedIn);
+  // const isLoggedIn = req.get('cookie').split('=')[1] === 'true';
+  // console.log(isLoggedIn);
+  console.log(req.session);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
   // req.isLoggedIn = true;
-  res.setHeader('Set-Cookie', 'LoggedIn=true');
+  // res.setHeader('Set-Cookie', 'LoggedIn=true');
+  req.session.isLoggedIn = true;
   res.redirect('/');
 };
