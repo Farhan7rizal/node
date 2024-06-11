28d94aef450c0e1b824a22b50ce4b2603813182d
Farhan7rizal <Farhan7rizal@gmail.com>
2024-06-11 10:00:44 +0700

    validiting product (post)

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,6 +2,7 @@ const Product = require('../models/product');
 const mongodb = require('mongodb');
 const { ObjectId } = require('mongodb');
 const product = require('../models/product');
+const { check, validationResult } = require('express-validator');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -9,6 +10,8 @@ exports.getAddProduct = (req, res, next) => {
     path: '/admin/add-product',
     editing: false,
     isAuthenticated: req.session.isLoggedIn,
+    hasError: false,
+    errorMessage: null,
   });
 };
 
@@ -17,14 +20,32 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        imageUrl: imageUrl,
+        price: price,
+        description: description,
+      },
+      errorMessage: errors.array()[0].msg,
+      isAuthenticated: req.session.isLoggedIn,
+    });
+  }
   const product = new Product({
     title: title,
+    imageUrl: imageUrl,
     price: price,
     description: description,
-    imageUrl: imageUrl,
     userId: req.user,
   });
-
   product
     .save()
     .then((result) => {
@@ -39,6 +60,7 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
+  const errors = validationResult(req);
   if (!editMode) {
     return res.redirect('/');
   }
@@ -53,6 +75,8 @@ exports.getEditProduct = (req, res, next) => {
         path: '/admin/edit-product',
         editing: editMode,
         product: product,
+        hasError: false,
+        errorMessage: null,
         isAuthenticated: req.session.isLoggedIn,
       });
     })
